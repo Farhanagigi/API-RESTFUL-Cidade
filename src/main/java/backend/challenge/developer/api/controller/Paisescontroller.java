89c046539cb17b51a50e0f527331e655306eccad
Farhan Agigi <88557261+Farhanagigi@users.noreply.github.com>
2023-01-18 15:59:04 +0200

    Fix: Upercaseclass and insert db Transactional

--- src/main/java/backend/challenge/developer/api/controller/Paisescontroller.java
+++ src/main/java/backend/challenge/developer/api/controller/Paisescontroller.java
@@ -1,6 +1,10 @@
 package backend.challenge.developer.api.controller;
 
 import backend.challenge.developer.api.pais.DadosCriacaoPais;
+import backend.challenge.developer.api.pais.Pais_table;
+import backend.challenge.developer.api.pais.Paisrepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/paises")
-public class paises {
+public class Paisescontroller {
 
+    @Autowired
+    private Paisrepository repository;
+
+    //criar um novo país a partir da API criada com todas as suas propriedades e salvar dados
     @PostMapping
+    @Transactional
     public void criacaopais(@RequestBody DadosCriacaoPais dados)
     {
-        System.out.println(dados);
+        repository.save(new Pais_table(dados));
     }
 
 }
